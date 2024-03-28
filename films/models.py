052dae90f8772144a6417660deddf280740a3a51
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-28 17:27:53 +0200

    Update Ordering with Lower

--- films/models.py
+++ films/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.functions import Lower
 
 class User(AbstractUser):
     pass
@@ -11,3 +12,6 @@ class Film(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = [Lower('name')]
